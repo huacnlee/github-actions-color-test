5afffe91b20254bdce45ead5345c1195c497e7a1
Jason Lee <huacnlee@gmail.com>
2022-12-11 00:30:42 +0800

    Add more test

--- src/ansi_term.rs
+++ src/ansi_term.rs
@@ -1,6 +1,5 @@
-use ansi_term::Colour::{Black, Green, Red, Yellow};
+use ansi_term::Colour::{Black, Blue, Cyan, Green, Red, Yellow};
 use ansi_term::Style;
-use owo_colors::OwoColorize;
 
 pub fn test() {
     println!("\n---------------- ansi_term ----------------");
@@ -17,13 +16,28 @@ pub fn test() {
     );
 
     println!(
+        "How about some {} and {}?",
+        Style::new().bold().paint("bold"),
+        Style::new().underline().paint("underline")
+    );
+
+    println!(
+        "Yellow on blue: {}",
+        Style::new().on(Blue).fg(Yellow).paint("yow!")
+    );
+    println!(
+        "Also yellow on blue: {}",
+        Cyan.on(Blue).fg(Yellow).paint("zow!")
+    );
+
+    println!(
         "{} {} {}",
         Red.paint("Red"),
         Green.paint("Green"),
         Yellow.paint("Yellow")
     );
     println!("{}", Red.on(Red).fg(Black).paint("Red BG"));
-    println!("{}", Green.on(Green).fg(Black).paint("Green BG"));
+    println!("{}", Green.on(Green).fg(Black).blink().paint("Green BG"));
     println!("{}", Yellow.on(Yellow).fg(Black).paint("Yellow BG"));
     println!("");
     println!(
@@ -48,8 +62,33 @@ pub fn test() {
     println!(
         "{}{}{}",
         Yellow.paint("Hello"),
-        Style::new().on(Yellow).fg(Black).paint(" "),
+        Style::new().on(Yellow).fg(Black).blink().paint(" "),
         Yellow.paint("World")
+    );
+    println!("---------------------");
+    println!(
+        "{}{}{}",
+        Red.paint("Hello"),
+        Style::new().on(Red).fg(Black).underline().paint(" "),
+        Red.paint("underline")
+    );
+    println!(
+        "{}{}{}",
+        Blue.paint("Hello"),
+        Style::new().on(Blue).fg(Black).dimmed().paint(" "),
+        Blue.paint("dimmed")
+    );
+    println!(
+        "{}{}{}",
+        Green.paint("Hello"),
+        Style::new().on(Green).fg(Black).blink().paint(" "),
+        Green.paint("blink")
+    );
+    println!(
+        "{}{}{}",
+        Cyan.paint("Hello"),
+        Style::new().on(Cyan).fg(Black).italic().paint(" "),
+        Cyan.paint("italic")
     );
     println!(
         "{}{}{}",
