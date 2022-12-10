3f0513c73d54c85d870a70510090f53019822383
Jason Lee <huacnlee@gmail.com>
2022-12-10 23:54:11 +0800

    Add more case

--- src/ansi_term.rs
+++ src/ansi_term.rs
@@ -1,5 +1,6 @@
 use ansi_term::Colour::{Black, Green, Red, Yellow};
 use ansi_term::Style;
+use owo_colors::OwoColorize;
 
 pub fn test() {
     println!("\n---------------- ansi_term ----------------");
@@ -25,28 +26,29 @@ pub fn test() {
     println!("{}", Green.on(Green).fg(Black).paint("Green BG"));
     println!("{}", Yellow.on(Yellow).fg(Black).paint("Yellow BG"));
     println!("");
-    // println!(
-    //     "{}{}{}",
-    //     "Hello".color(Red),
-    //     "  ".on_red(),
-    //     "World".color(Red)
-    // );
-    // println!(
-    //     "{}{}{}",
-    //     "Hello".color(Red),
-    //     " ".on_color(Red),
-    //     "World".color(Red)
-    // );
-    // println!(
-    //     "{}{}{}",
-    //     "Hello".color(Green),
-    //     " ".on_color(Green),
-    //     "World".color(Green)
-    // );
-    // println!(
-    //     "{}{}{}",
-    //     "Hello".color(Yellow),
-    //     " ".on_color(Yellow),
-    //     "World".color(Yellow)
-    // );
+    println!(
+        "{}{}{}",
+        Red.paint("Hello"),
+        Style::new().underline().fg(Red).paint(" "),
+        Red.paint("World")
+    );
+
+    println!(
+        "{}{}{}",
+        Red.paint("Hello"),
+        Style::new().on(Red).paint(" "),
+        Red.paint("World")
+    );
+    println!(
+        "{}{}{}",
+        Green.paint("Hello"),
+        Style::new().on(Green).fg(Black).paint(" "),
+        Green.paint("World")
+    );
+    println!(
+        "{}{}{}",
+        Yellow.paint("Hello"),
+        Style::new().on(Yellow).fg(Black).paint(" "),
+        Yellow.paint("World")
+    );
 }
